3cd5f1ad64f8f61e13468e6a9649be92832e1a8b
Stanley98745 <35321186+stanley98745@users.noreply.github.com>
2020-05-29 18:04:27 +0800

    Add files via upload

--- yolo_TF.py
+++ yolo_TF.py
@@ -4,33 +4,68 @@
 #
 #  Author       : Stanley J. F. Zhang
 #  Created Date : 2020-05-29
-#  Description  : Classify the dog image using yolov3 and tensorflow.
+#  Description  : Classify the image using yolov3 and tensorflow.
 #                 The performance will be compared with opencv dnn
 #                 module.
-#  Reference    : https://github.com/YunYang1994/tensorflow-yolov3
+#  Reference    : https://github.com/Cuda-Chen/opencv-dnn-cuda-test
+#                 https://github.com/YunYang1994/tensorflow-yolov3
 #
 ########################################################################
 
 
-from PIL import Image
 import numpy as np
 import tensorflow as tf
 import argparse
 import time
 import cv2
 
-import core.utils as utils
+
+def imgpreprocess(image, target_size, gt_boxes=None):
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+
+    ih, iw    = target_size
+    h,  w, _  = image.shape
+
+    scale = min(iw/w, ih/h)
+    nw, nh  = int(scale * w), int(scale * h)
+    image_resized = cv2.resize(image, (nw, nh))
+
+    image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0)
+    dw, dh = (iw - nw) // 2, (ih-nh) // 2
+    image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
+    image_paded = image_paded / 255.
+
+    if gt_boxes is None:
+        return image_paded
+
+    else:
+        gt_boxes[:, [0, 2]] = gt_boxes[:, [0, 2]] * scale + dw
+        gt_boxes[:, [1, 3]] = gt_boxes[:, [1, 3]] * scale + dh
+        return image_paded, gt_boxes
+
+
+def read_pb_return_tensors(graph, pb_file, return_elements):
+
+    with tf.gfile.FastGFile(pb_file, 'rb') as f:
+        frozen_graph_def = tf.GraphDef()
+        frozen_graph_def.ParseFromString(f.read())
+
+    with graph.as_default():
+        return_elements = tf.import_graph_def(frozen_graph_def, return_elements=return_elements)
+    return return_elements
+
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
-    ap.add_argument("-y", "--yolo", required=True,
-                    help="path to yolo-coco folder")
-    ap.add_argument("-i", "--image", required=True,
+    ap.add_argument("-y", "--yolo", default="./yolo-coco",
+                    help="path to yolo-coco.pb folder")
+    ap.add_argument("-i", "--image", default="./doc/image/dog.jpg",
                     help="path to image file")
     ap.add_argument("-g", "--gpu_utility", type=float, default=0.5,
                     help="reduce gpu memory utility to prevent from KILLED")
-    ap.add_argument("-t", "--times", type=int, required=True, default=100,
-                    help="testing loop for image")
+    ap.add_argument("-t", "--times", type=int, default=100,
+                    help="loop for testing")
     args = vars(ap.parse_args())
 
     '''Parameter setting'''
@@ -47,17 +82,14 @@ if __name__ == '__main__':
     original_image = cv2.imread(args["image"])
     original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
     original_image_size = original_image.shape[:2]
-    image_data = utils.image_preporcess(np.copy(original_image),
-                                        [input_size, input_size])
+    image_data = imgpreprocess(np.copy(original_image), [input_size, input_size])
     image_data = image_data[np.newaxis, ...]
 
     '''Tensorflow setting'''
     graph = tf.Graph()
     gpu_utility = tf.GPUOptions(per_process_gpu_memory_fraction=args["gpu_utility"])
     config = tf.ConfigProto(gpu_options=gpu_utility)
-    return_tensors = utils.read_pb_return_tensors(graph,
-                                                  pb_file,
-                                                  return_elements)
+    return_tensors = read_pb_return_tensors(graph, pb_file, return_elements)
 
     '''LOOP'''
     with tf.Session(config=config, graph=graph) as sess:
@@ -68,17 +100,6 @@ if __name__ == '__main__':
                 feed_dict={return_tensors[0]: image_data})
         end = time.time()
 
-    if args["display"] is True:
-        pred_bbox = np.concatenate([np.reshape(pred_sbbox, (-1, 5 + num_classes)),
-                                    np.reshape(pred_mbbox, (-1, 5 + num_classes)),
-                                    np.reshape(pred_lbbox, (-1, 5 + num_classes))], axis=0)
-
-        bboxes = utils.postprocess_boxes(pred_bbox, original_image_size, input_size, 0.3)
-        bboxes = utils.nms(bboxes, 0.5, method='nms')
-        image = utils.draw_bbox(original_image, bboxes)
-        image = Image.fromarray(image)
-        image.show()
-
-    ms_per_image = (end - start) * 1000/100
+    ms_per_image = (end - start) * 1000/args["times"]
     print(f'Time per inference: {ms_per_image} ms')
     print(f'FPS: {1000/ms_per_image}')
--- yolo_dnn.py
+++ yolo_dnn.py
@@ -4,34 +4,57 @@
 #
 #  Author       : Stanley J. F. Zhang
 #  Created Date : 2020-05-29
-#  Description  : Classify the dog image using yolov3 and opencv DNN.
+#  Description  : Classify the image using yolov3 and opencv DNN.
 #                 The performance will be compared with tensorflow.
 #
 #  Reference    : https://github.com/Cuda-Chen/opencv-dnn-cuda-test
+#                 https://github.com/YunYang1994/tensorflow-yolov3
 #
 ########################################################################
 
 
-from PIL import Image
 import numpy as np
 import argparse
 import time
 import cv2
 
-import core.utils as utils
+
+def imgpreprocess(image, target_size, gt_boxes=None):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+
+    ih, iw = target_size
+    h, w, _ = image.shape
+
+    scale = min(iw / w, ih / h)
+    nw, nh = int(scale * w), int(scale * h)
+    image_resized = cv2.resize(image, (nw, nh))
+
+    image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0)
+    dw, dh = (iw - nw) // 2, (ih - nh) // 2
+    image_paded[dh:nh + dh, dw:nw + dw, :] = image_resized
+    image_paded = image_paded / 255.
+
+    if gt_boxes is None:
+        return image_paded
+
+    else:
+        gt_boxes[:, [0, 2]] = gt_boxes[:, [0, 2]] * scale + dw
+        gt_boxes[:, [1, 3]] = gt_boxes[:, [1, 3]] * scale + dh
+        return image_paded, gt_boxes
+
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
-    ap.add_argument("-y", "--yolo", required=True,
+    ap.add_argument("-y", "--yolo", default="./yolo-coco",
                     help="path to yolo weight, cfg folder")
-    ap.add_argument("-i", "--image", required=True,
+    ap.add_argument("-i", "--image", default="./doc/image/dog.jpg",
                     help="path to image file")
-    ap.add_argument("-t", "--times", type=int, default=100,
-                    help="testing loop for image")
+    ap.add_argument("-t", "--times", type=int,
+                    help="loop for testing")
     args = vars(ap.parse_args())
 
-    yoloWeights = args["yolo"] + "yolov3.weights"
-    yoloConfig = args["yolo"] + "yolov3.cfg"
+    yoloWeights = args["yolo"] + "/yolov3.weights"
+    yoloConfig = args["yolo"] + "/yolov3.cfg"
 
     num_classes = 80
     input_size = 416
@@ -46,19 +69,17 @@ if __name__ == '__main__':
     original_image = cv2.imread(args["image"])
     original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
     original_image_size = original_image.shape[:2]
-    image_data = utils.image_preporcess(np.copy(original_image),
-                                        [input_size, input_size])
+    image_data = imgpreprocess(np.copy(original_image), [input_size, input_size])
     image_data = image_data[np.newaxis, ...]
     blob = cv2.dnn.blobFromImage(original_image, 0.00392, (416, 416), [0, 0, 0], True, False)
 
-    # benchmark
+    '''Benchmarking'''
     start = time.time()
     for i in range(args["times"]):
         net.setInput(blob)
         pred_sbbox, pred_mbbox, pred_lbbox = net.forward(ln)
     end = time.time()
 
-    ms_per_image = (end - start) * 1000/args["time"]
+    ms_per_image = (end - start) * 1000 / args["times"]
     print(f'Time per inference: {ms_per_image} ms')
-    print(f'FPS: {1000/ms_per_image}')
-
+    print(f'FPS: {1000 / ms_per_image}')
